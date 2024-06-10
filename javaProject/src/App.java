7d088a0c2cbb7c56e6f523f5bfb350eb3d066b05
Thiago <thiago.lanzarin2812@gmail.com>
2024-06-10 19:46:17 -0300

    implementando menu

--- javaProject/src/App.java
+++ javaProject/src/App.java
@@ -4,12 +4,34 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         final String MENU = "1- Adcionar pessoal\n  2- Listar pessoal\n  3- Alterar\n 4- Excluir\n 5- Sair\n";
+        boolean loop = true;
 
-        while (true) { 
+        while (loop) { 
             Scanner scanner = new Scanner(System.in);
 
             System.out.println(MENU);
-            scanner.nextInt();
+            int escolhaUser = scanner.nextInt();
+
+            switch(escolhaUser){
+                case 1:
+                    System.out.println("Adcionar pessoal");
+                    break;
+                case 2:
+                    System.out.println("Listar pessoal");
+                    break;
+                case 3:
+                    System.out.println("Alterar");
+                    break;
+                case 4:
+                    System.out.println("Excluir");
+                    break;
+                case 5:
+                    loop = false;
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+                    break;
+            }
         }
 
     }
